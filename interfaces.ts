8ec17b690e3ac187fec1b8754470759aff5e2229
Daniel Zeljko <danielzeljko@users.noreply.github.com>
2023-01-30 12:16:28 -0800

    save and switch

---
+++ interfaces.ts
@@ -0,0 +1,11 @@
+export interface ShowInterface {
+  id: number;
+  name: string;
+  summary: string;
+  image: string;
+}
+
+export interface ShowInterface2 {
+  score: number;
+  show: object;
+}
--- tvmaze.ts
+++ tvmaze.ts
@@ -1,10 +1,10 @@
 import axios from "axios";
 import * as $ from 'jquery';
+import { ShowInterface } from "./interfaces";
 
 const $showsList = $("#showsList");
 const $episodesArea = $("#episodesArea");
 const $searchForm = $("#searchForm");
-
 
 /** Given a search term, search for tv shows that match that query.
  *
@@ -13,40 +13,29 @@ const $searchForm = $("#searchForm");
  *    (if no image URL given by API, put in a default image URL)
  */
 
-async function getShowsByTerm(term) {
-  // ADD: Remove placeholder & make request to TVMaze search shows API.
-  return [
-    {
-      id: 1767,
-      name: "The Bletchley Circle",
-      summary:
-        `<p><b>The Bletchley Circle</b> follows the journey of four ordinary
-           women with extraordinary skills that helped to end World War II.</p>
-         <p>Set in 1952, Susan, Millie, Lucy and Jean have returned to their
-           normal lives, modestly setting aside the part they played in
-           producing crucial intelligence, which helped the Allies to victory
-           and shortened the war. When Susan discovers a hidden code behind an
-           unsolved murder she is met by skepticism from the police. She
-           quickly realises she can only begin to crack the murders and bring
-           the culprit to justice with her former friends.</p>`,
-      image:
-          "http://static.tvmaze.com/uploads/images/medium_portrait/147/369403.jpg"
-    }
-  ]
+async function getShowsByTerm(term: string): Promise<[ShowInterface]> {
+  const response = await axios.get(`https://api.tvmaze.com/search/shows?q=${term}`);
+
+  console.log("resp", response)
+
+
+  // return response.data;
 }
 
 
 /** Given list of shows, create markup for each and to DOM */
 
-function populateShows(shows) {
+function populateShows(shows:[ShowInterface]) {
   $showsList.empty();
 
-  for (let show of shows) {
+  for (const show of shows) {
+    console.log("show=", show)
+
     const $show = $(
-        `<div data-show-id="${show.id}" class="Show col-md-12 col-lg-6 mb-4">
+      `<div data-show-id="${show.id}" class="Show col-md-12 col-lg-6 mb-4">
          <div class="media">
            <img
-              src="http://static.tvmaze.com/uploads/images/medium_portrait/160/401704.jpg"
+              src=""
               alt="Bletchly Circle San Francisco"
               class="w-25 me-3">
            <div class="media-body">
@@ -60,7 +49,8 @@ function populateShows(shows) {
        </div>
       `);
 
-    $showsList.append($show);  }
+    $showsList.append($show);
+  }
 }
 
 
@@ -69,9 +59,8 @@ function populateShows(shows) {
  */
 
 async function searchForShowAndDisplay() {
-  const term = $("#searchForm-term").val();
+  const term = $("#searchForm-term").val() as string;
   const shows = await getShowsByTerm(term);
-
   $episodesArea.hide();
   populateShows(shows);
 }
